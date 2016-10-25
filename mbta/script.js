74e1812fbedd6bfae0b25f58ccc312144e549747
yashgurd <yasgur@bergen.org>
2016-10-24 20:50:22 -0400

    Added pin images and made polylines

--- mbta/script.js
+++ mbta/script.js
@@ -26,7 +26,7 @@ var fields = {name: "Field Corner",lat: 42.300093,lon: -71.061667};
 var shawmut = {name: "Shawmut Station",lat: 42.29312583,lon: -71.06573796000001};
 var ashmont ={name: "Ashmont Station",lat: 42.284652,lon: -71.06448899999999};
 
-stations = {alewife, davis, porter, harvard, central, kendall, charles, park, downtown, south, broadway, andrew, jfk, northquincy, wollaston, quincycenter, quincyadams, braintree, savin, fields, shawmut, ashmont};
+var stations_array = [alewife, davis, porter, harvard, central, kendall, charles, park, downtown, south, broadway, andrew, jfk, northquincy, wollaston, quincycenter, quincyadams, braintree, savin, fields, shawmut, ashmont];
 
 function getLocation() {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -35,17 +35,107 @@ function getLocation() {
 function showPosition(position) {
 	myLat = position.coords.latitude;
     myLong = position.coords.longitude;
+    printMap();
 }
 
 function initMap() {
 	getLocation();
-    myLocation = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 42.3601, lng: -71.0589},
-	    zoom: 13
-	});
+}
+
+function printMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: myLat, lng: myLong},
 	    zoom: 13
 	});
+	var marker = new google.maps.Marker({
+	    position: {lat: myLat, lng: myLong},
+	    map: map,
+	    title: "My Location",
+	    icon: {
+	    	 size: new google.maps.Size(50, 50),
+            scaledSize: new google.maps.Size(50, 50),
+	    	url: 'my-location-pin.png'
+        }
+		});
+	addPins();
 }
+
+function addPins() {
+	stations_array.forEach(function(station) {
+		var marker = new google.maps.Marker({
+		    position: {lat: station.lat, lng: station.lon},
+		    map: map,
+		    title: station.name,
+		    icon: {
+	    		size: new google.maps.Size(50, 50),
+            	scaledSize: new google.maps.Size(50, 50),
+	    		url: 'red-line-pin.png'
+        	}
+		});
+	});
+	addPolylines();
+}
+
+function addPolylines() {
+	var polylineCoordinates = [];
+	var leftSplit = [{lat: 42.320685, lng: -71.052391}];
+	var rightSplit = [];
+	var i = 0;
+	var j = 12;
+	var k = 18;
+	while (stations_array[i].name != "North Quincy Station") {
+        polylineCoordinates.push({lat: stations_array[i].lat, lng: stations_array[i].lon});
+        i++;
+    }
+    while (stations_array[j].name != "Savin Hill Station") {
+        rightSplit.push({lat: stations_array[j].lat, lng: stations_array[j].lon});
+        j++;
+    }
+    while (k < stations_array.length) {
+        leftSplit.push({lat: stations_array[k].lat, lng: stations_array[k].lon});
+        k++;
+    }
+    var stationPath = new google.maps.Polyline({
+          path: polylineCoordinates,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+    var stationPathLeft = new google.maps.Polyline({
+          path: leftSplit,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+    var stationPathRight = new google.maps.Polyline({
+          path: rightSplit,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+    stationPath.setMap(map);
+    stationPathLeft.setMap(map);
+    stationPathRight.setMap(map);
+    makeRequest();
+}
+
+function makeRequest() {
+	request = new XMLHttpRequest();
+ 	request.open("get", "https://rocky-taiga-26352.herokuapp.com/redline.json", true);
+ 	if (request.readyState == 4 && request.status == 200){
+		theData = request.responseText;
+		sched = JSON.parse(theData);
+	} 
+	else if (request.readyState == 4) {
+	    request.open("get", "https://rocky-taiga-26352.herokuapp.com/redline.json", true);
+		request.send();
+	}
+ 	request.send();
+}
+
+
+
 
