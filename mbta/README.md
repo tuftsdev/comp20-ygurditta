90bbb7537ea7c903aef89068ffc28d25260858ae
yashgurd <yasgur@bergen.org>
2016-10-25 22:24:48 -0400

    troubleshooted the infowindow - now working. added README

---
+++ mbta/README.md
@@ -0,0 +1,11 @@
+## Assignment 2: MTBA
+
+I have correctly implement the request for the MBTA JSON file that was then parsed correctly.
+I was able to create polylines and insert markers with custom icons on the map using Google Maps API.
+I had a difficult time with the markers' infowindows.
+
+I received help on this project from two TA's who helped me out a lot: Vincent and Elif 
+I also received some help with the JSON components from Doug since I missed that lecture. 
+
+I spent about 12 hours on this assignment. In this time, I learned how to deal with asyncronous function
+and became much more familiar dealing with JS objects and the Google Maps API.
--- mbta/script.js
+++ mbta/script.js
@@ -31,13 +31,13 @@ var ashmont ={name: "Ashmont",lat: 42.284652,lon: -71.06448899999999};
 var stations_array = [alewife, davis, porter, harvard, central, kendall, charles, park, downtown, south, broadway, andrew, jfk, northquincy, wollaston, quincycenter, quincyadams, braintree, savin, fields, shawmut, ashmont];
 
 function getLocation() {
-        navigator.geolocation.getCurrentPosition(showPosition);
+	navigator.geolocation.getCurrentPosition(showPosition);
 }
 
 function showPosition(position) {
 	myLat = position.coords.latitude;
-    myLong = position.coords.longitude;
-    printMap();
+	myLong = position.coords.longitude;
+	printMap();
 }
 
 function initMap() {
@@ -47,26 +47,26 @@ function initMap() {
 function printMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: myLat, lng: myLong},
-	    zoom: 13
+		zoom: 13
 	});
 	var marker = new google.maps.Marker({
-	    position: {lat: myLat, lng: myLong},
-	    map: map,
-	    title: "My Location",
-	    icon: {
-	    	size: new google.maps.Size(50, 50),
-            scaledSize: new google.maps.Size(50, 50),
-	    	url: 'my-location-pin.png'
-        }
+		position: {lat: myLat, lng: myLong},
+		map: map,
+		title: "My Location",
+		icon: {
+			size: new google.maps.Size(50, 50),
+			scaledSize: new google.maps.Size(50, 50),
+			url: 'my-location-pin.png'
+		}
 	});
 	var closestData = closestStation();
 	var infoBox = "<p>Closest station is " + closestData[0] + " and it is " + closestData[1] + " miles away.</p>";
 	var locationInfoBox = new google.maps.InfoWindow({
-        content: infoBox
-    });
-    marker.addListener('click', function() {
-        locationInfoBox.open(map, marker);
-    });
+		content: infoBox
+	});
+	marker.addListener('click', function() {
+		locationInfoBox.open(map, marker);
+	});
 	addPins();
 }
 
@@ -76,13 +76,13 @@ var infowindow;
 
 function addPins() {
 	request = new XMLHttpRequest();
-  	request.open("get", "https://rocky-taiga-26352.herokuapp.com/redline.json", true);
-  	request.onreadystatechange = function() {
-  				if (request.readyState == 4 && request.status == 200) {
-  					theData = request.responseText;
-    				schedInfo = JSON.parse(theData);
-					infoPins();
-				}
+	request.open("get", "https://rocky-taiga-26352.herokuapp.com/redline.json", true);
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			theData = request.responseText;
+			schedInfo = JSON.parse(theData);
+			infoPins();
+		}
 	};
 	request.send();
 	addPolylines();
@@ -90,42 +90,41 @@ function addPins() {
 
 function infoPins() {
 	for (var i=0; i < stations_array.length; i++) {
-					// received help with this loop
-					var marker = new google.maps.Marker({
-					    position: {lat: stations_array[i].lat, lng: stations_array[i].lon},
-					    map: map,
-					    title: stations_array[i].name,
-					    icon: {
-				    		size: new google.maps.Size(50, 50),
-			            	scaledSize: new google.maps.Size(50, 50),
-				    		url: 'red-line-pin.png'
-			        	}
-					});
-					schedInfo.TripList.Trips.forEach(function(train){
-					    train.Predictions.forEach(function(time){
-					      	if(time.Stop == stations_array[i].name) {
-					        	nextTrainInfo = "<p>The destination of next train is " + train.Destination + " in " + time.Seconds + " seconds.</p>";
-					      	}
-					    });
-					});		
-							    
-							  	infowindow = new google.maps.InfoWindow({
-							    	content: this.nextTrainInfo
-							    });
-							    console.log(nextTrainInfo);
-							  	google.maps.event.addListener(marker, 'click', function() {
-								    infowindow.open(map, this);
-								    // infowindow.setContent(nextTrainInfo);
-								    infowindow.setContent(this.nextTrainInfo);
-							  	});
+		// received help with this loop
+		var marker = new google.maps.Marker({
+			position: {lat: stations_array[i].lat, lng: stations_array[i].lon},
+			map: map,
+			title: stations_array[i].name,
+			icon: {
+				size: new google.maps.Size(50, 50),
+				scaledSize: new google.maps.Size(50, 50),
+				url: 'red-line-pin.png'
 			}
+		});
+		schedInfo.TripList.Trips.forEach(function(train){
+			train.Predictions.forEach(function(time){
+				if(time.Stop == stations_array[i].name) {
+					nextTrainInfo = "<p>The destination of next train is " + train.Destination + " in " + time.Seconds + " seconds.</p>";
+				}
+			});
+		});		
+
+		marker.nextTrainInfo = nextTrainInfo;
+		infowindow = new google.maps.InfoWindow({
+			content: this.nextTrainInfo
+		});
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map, this);
+			infowindow.setContent(this.nextTrainInfo);
+		});
+	}
 }
 
 function funex() {
 	if (request.readyState == 4 && request.status == 200) {
-  		theData = request.responseText;
-    	schedInfo = JSON.parse(theData);
-  	}
+		theData = request.responseText;
+		schedInfo = JSON.parse(theData);
+	}
 }
 
 function addPolylines() {
@@ -136,92 +135,92 @@ function addPolylines() {
 	var j = 12;
 	var k = 18;
 	while (stations_array[i].name != "North Quincy") {
-        polylineCoordinates.push({lat: stations_array[i].lat, lng: stations_array[i].lon});
-        i++;
-    }
-    while (stations_array[j].name != "Savin Hill") {
-        rightSplit.push({lat: stations_array[j].lat, lng: stations_array[j].lon});
-        j++;
-    }
-    while (k < stations_array.length) {
-        leftSplit.push({lat: stations_array[k].lat, lng: stations_array[k].lon});
-        k++;
-    }
-    var stationPath = new google.maps.Polyline({
-          path: polylineCoordinates,
-          geodesic: true,
-          strokeColor: '#FF0000',
-          strokeOpacity: 1.0,
-          strokeWeight: 2
-        });
-    var stationPathLeft = new google.maps.Polyline({
-          path: leftSplit,
-          geodesic: true,
-          strokeColor: '#FF0000',
-          strokeOpacity: 1.0,
-          strokeWeight: 2
-        });
-    var stationPathRight = new google.maps.Polyline({
-          path: rightSplit,
-          geodesic: true,
-          strokeColor: '#FF0000',
-          strokeOpacity: 1.0,
-          strokeWeight: 2
-        });
-    stationPath.setMap(map);
-    stationPathLeft.setMap(map);
-    stationPathRight.setMap(map);
+		polylineCoordinates.push({lat: stations_array[i].lat, lng: stations_array[i].lon});
+		i++;
+	}
+	while (stations_array[j].name != "Savin Hill") {
+		rightSplit.push({lat: stations_array[j].lat, lng: stations_array[j].lon});
+		j++;
+	}
+	while (k < stations_array.length) {
+		leftSplit.push({lat: stations_array[k].lat, lng: stations_array[k].lon});
+		k++;
+	}
+	var stationPath = new google.maps.Polyline({
+		path: polylineCoordinates,
+		geodesic: true,
+		strokeColor: '#FF0000',
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+	});
+	var stationPathLeft = new google.maps.Polyline({
+		path: leftSplit,
+		geodesic: true,
+		strokeColor: '#FF0000',
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+	});
+	var stationPathRight = new google.maps.Polyline({
+		path: rightSplit,
+		geodesic: true,
+		strokeColor: '#FF0000',
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+	});
+	stationPath.setMap(map);
+	stationPathLeft.setMap(map);
+	stationPathRight.setMap(map);
 }
 
 function closestStation() {
 	var myLoc = [myLat, myLong];
 	var closestStation = stations_array[0];
 	var shortestDistance = getDistance([stations_array[0].lat, stations_array[0].lon], myLoc);
-  	for (var i=1; i < stations_array.length; i++) {
-	    stationDistance = getDistance([stations_array[i].lat, stations_array[i].lon], myLoc);
-	    if (stationDistance < shortestDistance) {
-	      shortestDistance = stationDistance;
-	      closestStation = stations_array[i];
-	    }
-  	}
+	for (var i=1; i < stations_array.length; i++) {
+		stationDistance = getDistance([stations_array[i].lat, stations_array[i].lon], myLoc);
+		if (stationDistance < shortestDistance) {
+			shortestDistance = stationDistance;
+			closestStation = stations_array[i];
+		}
+	}
 
-  	closestStationPolyline = new google.maps.Polyline({
-	    path:[{lat: myLat, lng: myLong}, {lat:closestStation.lat, lng:closestStation.lon}],
-	    strokeColor: '#0000FF',
-        strokeOpacity: 1.0,
-        strokeWeight: 3
-  	});
+	closestStationPolyline = new google.maps.Polyline({
+		path:[{lat: myLat, lng: myLong}, {lat:closestStation.lat, lng:closestStation.lon}],
+		strokeColor: '#0000FF',
+		strokeOpacity: 1.0,
+		strokeWeight: 3
+	});
+	
+	closestStationPolyline.setMap(map);
 
-  	closestStationPolyline.setMap(map);
-
-  	return [closestStation.name, shortestDistance];
+	return [closestStation.name, shortestDistance];
 }
 
 // Resource: http://stackoverflow.com/questions/14560999/using-the-haversine-formula-in-javascript
 function getDistance(location1, location2) {
-  	Number.prototype.toRad=function(){
-    	return this*Math.PI/180;
-  	}
+	Number.prototype.toRad=function(){
+		return this*Math.PI/180;
+	}
 
-  	var lat1 = location1[0];
-  	var lon1 = location1[1];
-  	var lat2 = location2[0];
-  	var lon2 = location2[1];
+	var lat1 = location1[0];
+	var lon1 = location1[1];
+	var lat2 = location2[0];
+	var lon2 = location2[1];
 
-  	var R=6371;
+	var R=6371;
 
- 	var x1 = lat2-lat1;
-    var dLat = x1.toRad();
-    var x2 = lon2-lon1;
-    var dLon = x2.toRad();
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
-        Math.sin(dLon/2) * Math.sin(dLon/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    // adjust for miles
-  	var d = R * c;
+	var x1 = lat2-lat1;
+	var dLat = x1.toRad();
+	var x2 = lon2-lon1;
+	var dLon = x2.toRad();
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+		Math.sin(dLon/2) * Math.sin(dLon/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	// adjust for miles
+	var d = R * c;
 
-  return d;
+	return d;
 }
 
 
